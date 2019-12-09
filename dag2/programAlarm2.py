d0e5ef8a051ebc0be4f8c9602326465301c470f1
Marwan Chahade <marwan.chahade@volvocars.com>
2019-12-09 07:51:49 +0100

    clean code for day2 second task

--- dag2/programAlarm2.py
+++ dag2/programAlarm2.py
@@ -3,18 +3,20 @@ pos = position
 pos[1]=noun
 pos[2]=verb
 """
-readList = [int(x) for x in open ('./intInput.txt').read().split(',')]
-#which pair of inputs results in optOutput
-
+readList = [int(x) for x in open('./adventPuzzle/dag2/intInput.txt').read().split(',')]
+# which pair of inputs results in optOutput
+pos = 0
 for noun in range(100):
     for verb in range(100):
         pos = [x for x in readList]
+        
         pos[1] = noun
         pos[2] = verb
+
         intRead = 0
         while True:
             operationCode = pos[intRead]
-            i1, i2, i3 = pos[intRead+1], pos[intRead+2],pos[intRead+3]
+            i1, i2, i3 = pos[intRead+1], pos[intRead+2], pos[intRead+3]
             if operationCode == 1:
                 pos[i3] = pos[i1]+pos[i2]
             elif operationCode == 2:
@@ -25,3 +27,4 @@ for noun in range(100):
             intRead += 4
         if pos[0] == 19690720:
             print(noun, verb)
+print(pos)
